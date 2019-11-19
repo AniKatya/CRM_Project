c013ccdc6c2663985155a94550f54fea69942550
Katya <lopukhina.ekaterina@gmail.com>
2019-11-19 14:14:42 +0200

    ready to deploy

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser')
 const api = require('./server/routes/api')
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost/client")
+mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/client")
 
 app.use('/', api)
 app.use(bodyParser.json())
@@ -13,6 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.listen(process.env.PORT || 5000, function() {
     console.log("Server running : port 5000")
